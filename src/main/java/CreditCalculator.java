531da42fa2b2537d7d66a87321308caa9133c0c7
antonyanchev <a.yanchev18@gmail.com>
2024-02-19 12:18:21 +0200

    Допълнителни задачи

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,16 +1,16 @@
 public class CreditCalculator {
     public static void main(String[] args){
-       //calculateCreditDetails("Антон Янчев", 19, 2000, 24);
+       calculateCreditDetails("Антон Янчев", 19, 50000, 24);
         //calculateCreditDetails("Петър Тодоров", 17, 3000, 10);
         //calculateCreditDetails("Къци Въпцаров", 25, 900, 12);
-        calculateCreditDetails("Тишката", 26, 900, 13);
+        //calculateCreditDetails("Тишката", 26, 900, 13);
     }
 
     private static void calculateCreditDetails(String fullName, int age, int amount, int months) {
         if (age < 18) {
             System.out.println("Трябва да имате навършени 18 години за да изтеглите кредит");
         } else {
-            if (amount < 1000) {
+            if (amount < 1000 || amount > 50000) {
                 if (months > 12) {
                     System.out.println("Не може да изтеглите сума под 1000 лева за период по-дълъг от 12 месеца");
                 } else {
@@ -24,7 +24,6 @@ public class CreditCalculator {
                 System.out.printf("Месечна вноска за сума от %d лева за %d месеца с лихва %.2f%% е %.2f лева.%n",
                         amount, months, interestRate * 100 , monthlyPayment);
             }
-
         }
     }
 
